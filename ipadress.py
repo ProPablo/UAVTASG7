f82923005aa0fe95ebb49fbdcb3db827090a3171
anhad <anhadrs@gmail.com>
2021-08-19 14:02:08 +1000

    fotn

--- ipadress.py
+++ ipadress.py
@@ -47,7 +47,7 @@ WIDTH = disp.width
 HEIGHT = disp.height
 
 # Text settings.
-font_size = 25
+font_size = 17
 font_path = os.path.abspath(
     os.path.join(
         os.path.dirname(__file__), 'Roboto-Thin.ttf'
